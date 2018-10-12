97f34cafa51951e351c833d0c6ca057d44bc2fbf
Stephane <2020shatgiskessell@dwight.edu>
2018-10-12 13:24:21 -0400

    loop error

--- PathPlanning.py
+++ PathPlanning.py
@@ -18,7 +18,7 @@ class Node:
         self.h = h
         self.g = g
         self.cost = h + g
-        
+
     def __repr__(self):
         return repr((self.xcoordinate, self.ycoordinate, self.h, self.g, self.cost))
 
@@ -52,7 +52,7 @@ def estimate_noise(I):
 boxsize = (math.pow(estimate_noise(edge),-0.708)* 112.32)
 matrix_icrement_width = int(width/int(boxsize))
 matrix_icrement_height = int(height/int(boxsize))
-Matrix =  [[0 for x in range(matrix_icrement_width)] for y in range(matrix_icrement_height)] 
+Matrix =  [[0 for x in range(matrix_icrement_width)] for y in range(matrix_icrement_height)]
 
 
 Nodes = []
@@ -63,7 +63,14 @@ closed_list = []
 starting_node = Node(xcoordinate = 0, ycoordinate = 15, h = 0, g = 0 )
 closed_list.append(starting_node)
 
-ending_node = Node(xcoordinate = 19, ycoordinate = 1, h = 0, g = 0 )
+ending_node = Node(xcoordinate = 17, ycoordinate = 18, h = 0, g = 0 )
+#Test endpoints include:
+#(17, 18)
+#(17, 25)
+#(19, 0)
+
+
+cv2.circle(roomimg,(ending_node.xcoordinate * int(boxsize), ending_node.ycoordinate* int(boxsize)), 5, (0,255,54), -1)
 
 
 #defines what are obstacles and what are not
@@ -81,25 +88,26 @@ for i in range (0,width, int(boxsize)):
         roi_avg_intensity = np.mean(roi_gray)
         #3. BASED ON THAT, SEE IF ROI IS AN OBSTACLE OR NOT
         #print(box_num,"(",i,",",j,")")
-        
+
         if roi_avg_intensity > cut_off_point:
             # if box_num < 200:
                 # print("roi_avg_intensity:", roi_avg_intensity)
-                
+
             #DRAW RECTANGLE AROUND OBSATCLE
             cv2.rectangle(roomimg, (i,j), (i+int(boxsize), j+int(boxsize)),(0,0,255))
             #store top left corneer of ROI in Matrx
             Matrix[int(j/int(boxsize))][int(i/int(boxsize))] = "o"
             boxesdrawn += 1
         else:
-            #store top left corneer of ROI in Matrx
+            #sRAW RECTANGLE AROUND CLEAR PATH
             cv2.rectangle(roomimg, (i,j), (i+int(boxsize), j+int(boxsize)),(0,255,255))
-            
-            
+
+
             #Make sure i and j are not switch up!
             #Creates Node if there is a clear path
-            h = math.sqrt(math.pow(i-ending_node.xcoordinate,2)+math.pow(j-ending_node.ycoordinate,2))
-            newNode = Node(xcoordinate = i, ycoordinate = j, h = h, g = 0 )
+            h = math.sqrt(math.pow((i/int(boxsize))-ending_node.xcoordinate,2)+math.pow((j/int(boxsize))-ending_node.ycoordinate,2))
+
+            newNode = Node(xcoordinate = i/int(boxsize), ycoordinate = j/int(boxsize), h = h, g = 0 )
 
            #Add node to Matrix
             Matrix[int(j/int(boxsize))][int(i/int(boxsize))] = newNode
@@ -107,149 +115,202 @@ for i in range (0,width, int(boxsize)):
 
 
         box_num += 1
-        
+
 def draw_line(x1,y1, x2,y2):
     #convert Matrix point to points on the image
-    x1 = x1 * int(boxsize)
+    x1 = int (x1 * int(boxsize))
     #deal with the center instead of the top left corner
     x1 = x1 + int(int(boxsize)/2)
-    
-    y1 = y1 * int(boxsize)
+
+    y1 = int (y1 * int(boxsize))
     y1 = y1 + int(int(boxsize)/2)
-    
-    x2 = x2 * int(boxsize)
+
+    x2 = int(x2 * int(boxsize))
     x2 = x2 + int(int(boxsize)/2)
 
-    y2 = y2 * int(boxsize)
+    y2 = int(y2 * int(boxsize))
     y2 = y2 + int(int(boxsize)/2)
 
 
     cv2.line(roomimg,(x1,y1), (x2,y2), (255,255,255), 2)
-
 #---------------------------------------------------------STUFF FOR A STAR ALGORITHM----------------------------------------------------------------------------
+#NEED TO FIUGRE OUT WHAT TO DO IF PATH DOES NOT WORK OUT
+ #        .--'''''''''--.
+#     .'      .---.      '.
+#    /    .-----------.    \
+#   /        .-----.        \
+#   |       .-.   .-.       |
+#   |      /   \ /   \      |
+#    \    | .-. | .-. |    /
+#     '-._| | | | | | |_.-'
+#         | '-' | '-' |
+#          \___/ \___/
+#       _.-'  /   \  `-._
+#     .' _.--|     |--._ '.
+#     ' _...-|     |-..._ '
+#            |     |
+#            '.___.'
+#              | |
+#             _| |_
+#
+#        |\  |  ---  -----
+#        | \ | |   |   |
+#        |  \|  ---    |
+#
+#    \        /  ---   ----  |   .   __
+#     \  /\  /  |   |  |  |  |/     |  |   ---
+#      \/  \/    ---   |     |\  |  |  |  |   |
+#                                          ----
+#                                              |
 
+
+
+k = 0
+p = 0
 
 def astar (node, gvalue):
-    
+    global k
+    k = k+1
+
+    global next_step
     #check to see if program has reached the end point
-    if node.xcoordinate == ending_node.xcoordinate:
-        if node.ycoordinate == ending_node.ycoordinate:
-            print ("Reached end node")
-            return
-    
-    x = node.xcoordinate
-    y = node.ycoordinate
-    
-    #check to see if end point has been reaches
-    if x == ending_node.xcoordinate:
-        if y == ending_node.ycoordinate:
-            return
-        
-        
+    if node.xcoordinate == ending_node.xcoordinate and node.ycoordinate == ending_node.ycoordinate:
+        print ("Reached end node")
+        return
+
+    x = int(node.xcoordinate)
+    y = int(node.ycoordinate)
+
+    print ("(x,y) = (", x ,",", y,")")
+
+
     possible_next_steps = []
-    
+
     #get all the possible next steps from given node
-    #PROBLEM HERE: ALL OF THESE ARE BEING RECONGIZED AS OBSTACLES!!!!
-    north = Matrix[x][y-1]
-    east = Matrix[x+1][y]
-    south = Matrix[x][y+1]
-    west = Matrix[x-1][y]
-    north_east = Matrix[x+1][y-1]
-    north_west = Matrix[x-1][y-1]
-    south_east = Matrix[x+1][y+1]
-    south_west = Matrix[x-1][y+1]
-    
+    #FOUND PROBLEM AIIIII - THIS IS BEING CALCULATED WRONG (THE COORIDNATES ARE NOT LIKE IN THE JAVA COORDINATE SYSTEM)
+    north = Matrix[y-1][x]
+    east = Matrix[y][x+1]
+    south = Matrix[y+1][x]
+    west = Matrix[y][x-1]
+    north_east = Matrix[y-1][x+1]
+    north_west = Matrix[y-1][x-1]
+    south_east = Matrix[y+1][x+1]
+    south_west = Matrix[y+1][x-1]
+
     #calculate g value of each node (BE CAREFUL WITH THE MATH HERE) and add to array
-    if north != "o":
-        north.g = gvalue + 10
+    if north != "o" and x >= 0 and (y-1) >=0:
+        north.g = 10
         possible_next_steps.append(north)
-        print ("north x is ", north.xcoordinate, "north y is ", north.ycoordinate)
-    
-    if east != "o":
-        east.g = gvalue + 10
+        #print ("north x is ", north.xcoordinate, "north y is ", north.ycoordinate)
+
+    if east != "o" and (x+1) >= 0 and y >=0:
+        east.g = 10
         possible_next_steps.append(east)
-        print ("east x is ", east.xcoordinate, "east y is ", east.ycoordinate)
+        #print ("east x is ", east.xcoordinate, "east y is ", east.ycoordinate)
 
-
-    if south != "o":
-        south.g = gvalue + 10
+    if south != "o" and x >= 0 and (y+1) >=0:
+        south.g = 10
         possible_next_steps.append(south)
-        print ("south x is ", south.xcoordinate, "south y is ", south.ycoordinate)
+        #print ("south x is ", south.xcoordinate, "south y is ", south.ycoordinate)
 
-
-    if west != "o":
-        west.g = gvalue + 10
+    if west != "o" and (x-1) >= 0 and (y) >=0:
+        west.g = 10
         possible_next_steps.append(west)
-        print ("west x is ", west.xcoordinate, "west y is ", west.ycoordinate)
+        #print ("west x is ", west.xcoordinate, "west y is ", west.ycoordinate)
 
-    if north_east != "o":
-        north_east.g = gvalue + 15
+    if north_east != "o" and (x+1) >= 0 and (y-1) >=0:
+        north_east.g = 15
         possible_next_steps.append(north_east)
-        print ("north east x is ", north_east.xcoordinate, "north east y is ", north_east.ycoordinate)
+        #print ("north east x is ", north_east.xcoordinate, "north east y is ", north_east.ycoordinate)
 
-    if north_west != "o":
-        north_west.g = gvalue + 15
+    if north_west != "o" and (x-1) >= 0 and (y-1) >=0:
+        north_west.g = 15
         possible_next_steps.append(north_west)
-        print ("north west x is ", north_west.xcoordinate, "north west y is ", north_west.ycoordinate)
+        #print ("north west x is ", north_west.xcoordinate, "north west y is ", north_west.ycoordinate)
 
-
-    if south_east != "o":
-        south_east.g = gvalue + 15
+    if south_east != "o" and (x+1) >= 0 and (y+1) >=0:
+        south_east.g = 15
         possible_next_steps.append(south_east)
-        print ("south east x is ", south_east.xcoordinate, "south east y is ", south_east.ycoordinate)
+        #print ("south east x is ", south_east.xcoordinate, "south east y is ", south_east.ycoordinate)
 
-    if south_west != "o":
-        south_west.g = gvalue + 15
+    if south_west != "o" and (x-1) >= 0 and (y+1) >=0:
+        south_west.g = 15
         possible_next_steps.append(south_west)
-        print ("south west x is ", south_west.xcoordinate, "south west y is ", south_west.ycoordinate)
+        #print ("south west x is ", south_west.xcoordinate, "south west y is ", south_west.ycoordinate)
+
+#check if there are any possible next steps
+    if not possible_next_steps:
+        print ("There are no possible next steps")
+        return
 
 #Find lowerst cost node and add that to closed_list
-    newlist = sorted(possible_next_steps, key=lambda x: x.cost)
-    closed_list.append(newlist[0])
+    possible_next_steps = sorted(possible_next_steps, key=lambda x: x.cost)
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+    #Make sure u are not going back to a node u just visited
+
+
+    if len(possible_next_steps)>0 and possible_next_steps[0]not in closed_list:
+        next_step = possible_next_steps[0]
+        closed_list.append(next_step)
+
+    elif len(possible_next_steps) > 1 and possible_next_steps[0] in closed_list:
+        #go to all the next steps that have not already been visited
+        for i in range (0, len(possible_next_steps)-1):
+            if possible_next_steps[i] not in closed_list:
+                next_step = possible_next_steps[i]
+                closed_list.append(next_step)
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+    #Make sure u r not going in the opposite direction (ie: if a dead end is reached)
+#    if node.cost != 0 and next_step.cost > node.cost:
+#        global p
+#        print ("(x,y) = (", x ,",", y,")")
+#        print ("dead end after (", node.xcoordinate, ",", node.ycoordinate, ")")
+#        #Go to the previous node if current node is at a dead end
+#        previous_node = closed_list[len(closed_list)-1]
+#        #print ("(previous x, previous y) = (", previous_node.xcoordinate ,",", previous_node.ycoordinate,")")
+#        p = p+1
+#        print ("paths taken: ", paths_taken)
+#        if p < 1:
+#            print ("calling a star, back_stepping = True")
+#            astar(previous_node, previous_node.g, True)
+#    else:
+#        paths_taken = paths_taken +1
+
+#---------------------------------------------------------------------------------------------------------------------------------------
+
+
 #Draw line between current node and the next step node
-    draw_line(x, y, x+1, y-1)
-#update gvalue'
-    gvalue = gvalue + newlist[0].g
-    #gvalue = gvalue + next_step.get(next_step)
-
-#repeat for next node
-    #astar(possible_next_steps[0], gvalue)
-
-
-
-    
-#A STAR TIME
-#1. Start with node N
-
-#2. Calculate distance between node N and its nearest neighbores in the NESW directions (possible add northeast, northwest, southeast, soutthwest)
-    #perhaps look at Matrix
-
-#3. Add that to h
-#4. Bubble sort lowerst cost node and add that to closed array
-#5. N = lowest cost node
-    
-    
-#FIGURE OUT WHAT TO DO AT END
+    draw_line(x, y, next_step.xcoordinate, next_step.ycoordinate)
+#update gvalue
+    gvalue = gvalue + next_step.g
+    #print ("cost: ", node.cost)
+    #print ("Coordinates of next step are: ",newlist[0].xcoordinate, ",", newlist[0].ycoordinate)
+#repeat for next node, but check if program is going backwards cus it reached a dead end
+    if k < 30:
+        astar(next_step, gvalue)
 
 
 #ALL OF THIS IS ONLY IF U WANT A STAR
 
-
 astar(starting_node, 0)
 #---------------------------------------------------------STUFF FOR A STAR ALGORITHM----------------------------------------------------------------------------
 
-
 def print_matrix():
     f = open('obstaclemap.txt', 'w')
-    f.write('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
+    f.write('\n'.join([''.join(['{:4}'.format(item) for item in row])
       for row in Matrix]))
     f.close()
 
 
-
+#print matrix
 #print_matrix()
 
+#save image file
 #cv2.imwrite('roomimg.jpg', roomimg)
+
+#display image
 cv2.imshow("image", roomimg)
 
 
